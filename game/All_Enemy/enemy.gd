9fce6b6e67919fbb01d1e7eadfad92bdad426869
sleo <leo.sautron@epitech.eu>
2023-02-10 21:54:44 +0400

    enemy

--- game/All_Enemy/enemy.gd
+++ game/All_Enemy/enemy.gd
@@ -11,18 +11,34 @@ enum {
 
 var stat = IDLE
 
+var gravity = -14
 var velocity = Vector3.ZERO
+
+func _ready():
+	pass
 
 func _physics_process(delta):
 	match stat:
 		IDLE:
 			seek_player()
+			
 		WANDER:
 			seek_player()
 		CHASE:
-			print("oui")
-			pass
+			var player = playerdection.player
+			var direction = global_transform.origin.direction_to(player.get_global_transform().origin)
+			velocity = velocity.move_toward(direction * 5, delta * 10)
+			
+			
+	velocity = move_and_slide(velocity)
 	
 func seek_player():
 	if playerdection.can_see_player():
 		stat = CHASE
+
+func pick_random_state(list_state):
+	list_state.shuffle()
+	return list_state.pop(0)
+
+func _on_HurtBox_area_entered(area):
+	pass
