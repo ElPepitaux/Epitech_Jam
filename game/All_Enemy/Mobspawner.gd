90d3e9b2b787804ada5118946fa27bcf959a679d
sleo <leo.sautron@epitech.eu>
2023-02-11 22:58:37 +0400

    spawn enemy

--- game/All_Enemy/Mobspawner.gd
+++ game/All_Enemy/Mobspawner.gd
@@ -4,6 +4,23 @@ onready var stats = $Stats
 onready var Enemy = preload("res://All_Enemy/enemy.tscn")
 onready var map = get_parent()
 onready var lot_1 = preload("res://items/Heal.tscn")
+onready var time = $Timer
+
+var spawn = false
+
+func _ready():
+	set_timer(randi() % 240)
+	
+func _process(delta):
+	if spawn:
+		spawn = false
+		var enemy = Enemy.instance()
+		enemy.global_transform = global_transform
+		map.add_child(enemy)
+		set_timer(randi() % 240)
+
+func set_timer(duration):
+	time.start(duration)
 
 func _on_HurtBox_area_entered(area):
 	stats.health -= area.damage
@@ -12,9 +29,11 @@ func _on_HurtBox_area_entered(area):
 		enemy.global_transform = global_transform
 		map.add_child(enemy)
 
-
 func _on_Stats_no_health():
 	var health = lot_1.instance()
 	health.global_transform = global_transform
 	map.add_child(health)
 	queue_free()
+
+func _on_Timer_timeout():
+	spawn = true
