1831b9fa3fda4b6c6a4fb4b6855bcfea128aa69d
Nathan <nathan.maillot@epitech.eu>
2023-02-11 18:35:17 +0400

    fixing bullets damage

--- game/WeaponStats.gd
+++ game/WeaponStats.gd
@@ -3,7 +3,7 @@ extends Node
 export (int) var max_ammo = 1
 onready var ammo = max_ammo setget shotting
 export (int) var SPEED = 1
-
+export (int) var damage = 1 
 signal no_ammo
 
 func shotting(value):
